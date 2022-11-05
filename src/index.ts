2d83d148e79772168cd7bbeb32178594dc0c1086
ozgur <ozgurkurucan337@gmail.com>
2022-11-05 23:18:52 +0300

    config update

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
 import {AutocompletePro} from './autocomplete-pro';
 import { sql } from './constants';
-import { useStateMachine } from './hooks'
+import { useStateMachine } from './hooks';
+import { useStateMachineProps } from "./types";
 export {
   AutocompletePro,
   sql,
   useStateMachine
+}
+
+export type {
+  useStateMachineProps
 }
