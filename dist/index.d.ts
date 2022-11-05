7381987920b0427acb78ec2fcb6d1dc260cb1185
ozgur <ozgurkurucan337@gmail.com>
2022-11-05 23:20:31 +0300

    index file conf

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,6 @@
 import { AutocompletePro } from './autocomplete-pro';
 import { sql } from './constants';
 import { useStateMachine } from './hooks';
+import { useStateMachineProps } from "./types";
 export { AutocompletePro, sql, useStateMachine };
+export type { useStateMachineProps };
